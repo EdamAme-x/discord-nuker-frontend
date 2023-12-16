c268bdc6f82341a514388e8cd36bb37a425a1df0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-16 17:00:20 +0900

    'add'

--- components/tool/tokenPanel.tsx
+++ components/tool/tokenPanel.tsx
@@ -103,7 +103,7 @@ import { MdDelete } from "react-icons/md";
 export function Setting(props: {
     children: React.ReactNode,
     data: Tokens,
-    setDate: (data: Tokens) => void
+    setData: (data: Tokens) => void
 }) {
 	return (
 		<Dialog>
@@ -116,11 +116,13 @@ export function Setting(props: {
 				</DialogHeader>
 				<div className="grid gap-4 py-4">
 					<Button className="inline-flex justify-center items-center" onClick={() => {
-                        const password = Math.random().toString(36).substring(2,6);
+                        const password = Math.random().toString(36).substring(2, 5);
                         const answer = prompt(`"${password}"を入力することで、Tokenは完全に削除されます。\n よろしければ入力して下さい。`) ?? "fail"
                         if (password !== answer) {
                             toast.error("文字列が違うか、入力されていません。")
                         }else {
+                            LS.remove("tokens")
+                            props.setData([])
                             toast.success("削除成功")
                         }
                     }}><MdDelete className="transform scale-150 mr-2" /> Clear All Token</Button>

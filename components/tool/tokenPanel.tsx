92630ad0c6a528ae1fa6a8f75fd56a7e935e21cc
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-16 17:20:07 +0900

    'add'

--- components/tool/tokenPanel.tsx
+++ components/tool/tokenPanel.tsx
@@ -48,7 +48,10 @@ export function TokenPanel(props: { data: Tokens; setData: (data: Tokens) => voi
 	return (
 		<div className="max-w-[370px] flex flex-col justify-center items-center">
 			<Label className="text-xl font-bold w-full px-[5px] inline-flex justify-center items-center my-3">
-				TokenPanel <Setting data={props.data} setData={props.setData}><IoSettingsSharp className="ml-auto" /></Setting>
+				TokenPanel{" "}
+				<Setting data={props.data} setData={props.setData}>
+					<IoSettingsSharp className="ml-auto" />
+				</Setting>
 			</Label>
 			<MultiSelect
 				options={props.data.map(i => {
@@ -100,32 +103,33 @@ import {
 } from "@/components/ui/dialog";
 import { MdDelete } from "react-icons/md";
 
-export function Setting(props: {
-    children: React.ReactNode,
-    data: Tokens,
-    setData: (data: Tokens) => void
-}) {
+export function Setting(props: { children: React.ReactNode; data: Tokens; setData: (data: Tokens) => void }) {
 	return (
 		<Dialog>
-			<DialogTrigger asChild>
-				{props.children}
-			</DialogTrigger>
+			<DialogTrigger asChild>{props.children}</DialogTrigger>
 			<DialogContent className="sm:max-w-[425px]">
 				<DialogHeader>
 					<DialogTitle>Settings</DialogTitle>
 				</DialogHeader>
 				<div className="grid gap-4 py-4">
-					<Button className="inline-flex justify-center items-center" onClick={() => {
-                        const password = Math.random().toString(36).substring(2, 5);
-                        const answer = prompt(`"${password}"を入力することで、Tokenは完全に削除されます。\n よろしければ入力して下さい。`) ?? "fail"
-                        if (password !== answer) {
-                            toast.error("文字列が違うか、入力されていません。")
-                        }else {
-                            LS.remove("tokens")
-                            props.setData([])
-                            toast.success("削除成功")
-                        }
-                    }}><MdDelete className="transform scale-150 mr-2" /> Clear All Token</Button>
+					<Button
+						className="inline-flex justify-center items-center"
+						onClick={() => {
+							const password = Math.random().toString(36).substring(2, 5);
+							const answer =
+								prompt(
+									`"${password}"を入力することで、Tokenは完全に削除されます。\n よろしければ入力して下さい。`
+								) ?? "fail";
+							if (password !== answer) {
+								toast.error("文字列が違うか、入力されていません。");
+							} else {
+								LS.remove("tokens");
+								props.setData([]);
+								toast.success("削除成功");
+							}
+						}}>
+						<MdDelete className="transform scale-150 mr-2" /> Clear All Token
+					</Button>
 				</div>
 			</DialogContent>
 		</Dialog>

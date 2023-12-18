34f524b3694f4b91aff79a29be87b4d6b3493788
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-18 22:54:29 +0900

    add code Parser

---
+++ lib/codeParser.ts
@@ -0,0 +1,8 @@
+export const codeParser = {
+    encode: (str: string): string => {
+        return btoa(encodeURIComponent(str)).split("").reverse().join("");
+    },
+    decode: (str: string): string => {
+        return atob(decodeURIComponent(str.split("").reverse().join("")));
+    }
+}

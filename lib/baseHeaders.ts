ae469489dd4d37fe5366dee81a8c6e4e83e8ec54
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-18 22:04:28 +0900

    'add'

---
+++ lib/baseHeaders.ts
@@ -0,0 +1,11 @@
+export const baseHeaders = {
+	"content-type": "application/json",
+	"sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+	"sec-ch-ua-mobile": "?0",
+	"sec-ch-ua-platform": '"Windows"',
+	"sec-fetch-dest": "empty",
+	"sec-fetch-mode": "cors",
+	"sec-fetch-site": "cross-site",
+	accept: "/",
+	"accept-language": "ja,en-US;q=0.9,en;q=0.8"
+};
--- lib/isWorks.ts
+++ lib/isWorks.ts
@@ -1,18 +1,11 @@
 // Token 生存確認用
+import { baseHeaders } from "./baseHeaders.ts";
 
 export async function isWorks(token: string): Promise<boolean> {
 	const resp = await fetch("https://discord.com/api/v9/users/@me/settings", {
 		headers: {
-			accept: "/",
-			"accept-language": "ja,en-US;q=0.9,en;q=0.8",
 			authorization: token,
-			"content-type": "application/json",
-			"sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-			"sec-ch-ua-mobile": "?0",
-			"sec-ch-ua-platform": '"Windows"',
-			"sec-fetch-dest": "empty",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-site": "cross-site"
+			...baseHeaders
 		},
 		referrer: "https://discord.com/channels/@me",
 		referrerPolicy: "strict-origin-when-cross-origin",
